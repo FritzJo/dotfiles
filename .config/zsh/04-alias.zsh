2f0d627448e709fe88f0a2be66e0b0395ffc25a1
FritzJo <fritzjo-git@mailbox.org>
2020-04-24 18:30:02 +0200

    added function to remove unused packages and run tmux in every new shell

--- .config/zsh/04-alias.zsh
+++ .config/zsh/04-alias.zsh
@@ -1,4 +1,5 @@
 alias pl="pacman -Qe | cut -d' ' -f1 | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
+alias pc="sudo pacman -Rns $(pacman -Qtdq)"
 alias vim="nvim"
 alias vi="nvim"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
