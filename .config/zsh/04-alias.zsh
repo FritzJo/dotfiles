0892649723fd470a955f84a1c3016b4ae615d49d
FritzJo <fritzjo-git@mailbox.org>
2020-05-08 22:03:30 +0200

    better function names

--- .config/zsh/04-alias.zsh
+++ .config/zsh/04-alias.zsh
@@ -1,5 +1,5 @@
-alias pl="pacman -Qe | cut -d' ' -f1 | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
-alias pc="sudo pacman -Rns $(pacman -Qtdq)"
+alias plist="pacman -Qe | cut -d' ' -f1 | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'"
+alias pclean="sudo pacman -Rns $(pacman -Qtdq)"
 alias vim="nvim"
 alias vi="nvim"
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
