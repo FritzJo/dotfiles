baa41a64ffb69423ba09a6c2f7bf83de2df5121c
FritzJo <fritzjo-git@mailbox.org>
2021-04-09 23:13:54 +0200

    improve init script

--- init.sh
+++ init.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+filename='packages.txt'
+
+# Check for root
+if [ "$EUID" -ne 0 ]
+    then echo "Please run this script as root"
+    exit
+fi
+
+# Update packet repo
 pacman -Syu
-pacman -S htop neovim neofetch tmux sway waybar swaylock swayidle zsh curl wget imv git kitty
+
+while read -r line; do
+    # reading each line
+    echo "Installing $line"
+    pacman -S "$line"
+done < $filename
